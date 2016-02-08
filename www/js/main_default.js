04c1839398e56aa7b85b13fd3aa0603868b5dfe6
Vaughn Dumas <vaughndumas@gmail.com>
2016-02-08 16:02:51 +1100

    Initial commit

---
+++ www/js/main_default.js
@@ -0,0 +1,56 @@
+var vg_client = "MLC Sydney";
+var vg_custcode = "MLCS";
+//var vg_base_url = "http://mlc.local";
+var vg_base_url = "http://mlc.tracking-intelligence.com";
+var vg_mapbox_token="pk.eyJ1IjoidmF1Z2huZHVtYXMiLCJhIjoiMGI5OWM2NWJjZDcxMjA1Nzc3MDQ4ODE4NjA5N2UxNDkifQ.JxG27h6rKURTY7mhtFrdng";
+var fi_icons = [];
+var icon_north = L.icon({
+    iconUrl: '/img/direction/n.png',
+    iconSize: [32, 37]
+});
+fi_icons['n.png'] = icon_north;
+var icon_northeast = L.icon({
+    iconUrl: '/img/direction/ne.png',
+    iconSize: [32, 37]
+});
+fi_icons['ne.png'] = icon_northeast;
+var icon_east = L.icon({
+    iconUrl: '/img/direction/e.png',
+    iconSize: [32, 37]
+});
+fi_icons['e.png'] = icon_east;
+var icon_southeast = L.icon({
+    iconUrl: '/img/direction/se.png',
+    iconSize: [32, 37]
+});
+fi_icons['se.png'] = icon_southeast;
+var icon_south = L.icon({
+    iconUrl: '/img/direction/s.png',
+    iconSize: [32, 37]
+});
+fi_icons['s.png'] = icon_south;
+var icon_southwest = L.icon({
+    iconUrl: '/img/direction/sw.png',
+    iconSize: [32, 37]
+});
+fi_icons['sw.png'] = icon_southwest;
+var icon_west = L.icon({
+    iconUrl: '/img/direction/w.png',
+    iconSize: [32, 37]
+});
+fi_icons['w.png'] = icon_west;
+var icon_northwest = L.icon({
+    iconUrl: '/img/direction/nw.png',
+    iconSize: [32, 37]
+});
+fi_icons['nw.png'] = icon_northwest;
+var icon_stop = L.icon({
+    iconUrl: '/img/direction/stop.png',
+    iconSize: [32, 37]
+});
+fi_icons['stop.png'] = icon_stop;
+var icon_go = L.icon({
+    iconUrl: '/img/direction/go.png',
+    iconSize: [10, 10]
+});
+fi_icons['go.png'] = icon_go;
